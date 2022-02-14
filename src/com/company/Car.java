670c0be887fd6073492d97530e8719e2f034bd86
RUQAYYAH MUSTAFA <ruqayyahmustafa@RUQAYYAHs-MacBook-Pro.local>
2022-02-14 14:15:52 -0600

    New project CarProgram added

---
+++ src/com/company/Car.java
@@ -0,0 +1,28 @@
+package com.company;
+
+public class Car {
+    //color, make, model, etc
+    //Create an assessor model for each
+    private String carMake;
+    private String carModel;
+    private String carColor;
+
+
+
+    //method to set the parameters
+
+    //set Make Parameter
+    public void setCarMake(String make)
+    {
+        String carMake = make;
+    }//end setCarMake
+
+    //get Make Parameter
+    private String getCarMake()
+    {
+        return carMake();
+    }
+
+
+
+}//end class Car
---
+++ src/com/company/CarDemo.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class CarDemo {
+
+    public static void main(String[] args) {
+	// write your code here
+        /*Create CarProgram using the Command Line App template
+    }
+}
